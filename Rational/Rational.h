8575006057b1cbb6c2acd79c98360084dfddecbf
Michael Hendrickson <michael.hendrickson@hbsw.co>
2016-10-24 20:22:45 -0700

    Initial Commit

---
+++ Rational/Rational.h
@@ -0,0 +1,19 @@
+//
+//  Rational.h
+//  Rational
+//
+//  Created by Michael Hendrickson on 10/24/16.
+//  Copyright © 2016 Michael Hendrickson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Rational.
+FOUNDATION_EXPORT double RationalVersionNumber;
+
+//! Project version string for Rational.
+FOUNDATION_EXPORT const unsigned char RationalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rational/PublicHeader.h>
+
+
