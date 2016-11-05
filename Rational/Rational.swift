98a3815b0ceb212c30a80b6d772c76138b406c32
Michael Hendrickson <michael.hendrickson@hbsw.co>
2016-11-04 18:57:32 -0700

    Completed Rational

---
+++ Rational/Rational.swift
@@ -0,0 +1,318 @@
+//
+//  Rational.swift
+//  Rational
+//
+//  Created by Michael Hendrickson on 10/24/16.
+//  Copyright © 2016 Michael Hendrickson. All rights reserved.
+//
+
+import Foundation
+
+/// Represents a rational number, maintained in reduced form.
+struct Rational {
+    /// The numerator
+    var numerator: Int;
+    
+    /// The denominator
+    var denominator: Int;
+
+    /// Initialize the Rational as a whole number
+    /// - Parameters:
+    ///     - numerator: The whole value to set to the Rational
+    /// - Note: The denominator is initialized to 1
+    init(_ numerator: Int) {
+        self.init(numerator, 1);
+    }
+    
+    /// Initialize the Rational as a whole number
+    /// - Parameters:
+    ///     - numerator: The "top" number of the Rational
+    ///     - denominator: The "bottom" number of the Rational
+    /// - Note: The number will be normalized to divide out the common divisor of the numerator and denominator.
+    ///         In addition, for negative numbers, the numerator will be negative. The denominator will always be 
+    ///         non-negative. A denominiator of 0 signifies an infinite value.
+    init(_ numerator: Int, _ denominator: Int) {
+        self.numerator = numerator;
+        self.denominator = denominator;
+        normalize();
+    }
+    
+    /// The multiplicative inverse of the the number.
+    /// - Note: Essentially, this will swap the numerator and denominator, leaving the numerator negative.
+    func inverse() -> Rational {
+        return Rational(denominator, numerator);
+    }
+    
+    /// Normalize the Rational by dividing out the common divisor of the numerator and denominator and, for negative
+    /// numbers, making the numerator negative and the denominator positive.
+    private mutating func normalize() {
+         if denominator < 0 {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+        
+        (numerator, denominator) = reducedTuple((numerator, denominator));
+    }
+    
+    /// Removes the common divisor from a tuple.
+    /// - Parameters:
+    ///     - t: A tuple of integers
+    /// - Returns: The tuple with the common divisor divided out
+    private func reducedTuple(_ t: (Int, Int)) -> (Int, Int) {
+        guard t != (0, 0) else {
+            return (0, 0);
+        }
+        
+        let gcd = Int.gcd(abs(t.0), abs(t.1));
+        return (t.0 / gcd, t.1 / gcd);
+    }
+    
+    /// Is the Rational finite?
+    /// Returns: False if the denominator is 0, True otherwise
+    func isFinite() -> Bool {
+        return denominator != 0;
+    }
+    
+    /// Is the Rational infinite?
+    /// Returns: True if the denominator is 0, False otherwise
+    func isInfinite() -> Bool {
+        return numerator != 0 && denominator == 0;
+    }
+    
+    /// Is the Rational not a number?
+    /// Returns: True if the numerator and denominator are 0, False otherwise
+    func isNaN() -> Bool {
+        return numerator == 0 && denominator == 0;
+    }
+}
+
+// MARK: IntegerArithmetic
+extension Rational : IntegerArithmetic {
+    static func + (lhs: Rational, rhs: Rational) -> Rational {
+        guard lhs.isFinite() && rhs.isFinite() else {
+            return Rational(lhs.numerator + rhs.numerator, 0);
+        }
+        
+        let lcm = Int.lcm(lhs.denominator, rhs.denominator);
+        return Rational((lhs.numerator * (lcm / lhs.denominator)) + (rhs.numerator * (lcm / rhs.denominator)), lcm);
+    }
+    
+    static func addWithOverflow(_ lhs: Rational, _ rhs: Rational) -> (Rational, overflow: Bool) {
+        guard lhs.isFinite() && rhs.isFinite() else {
+            return (Rational(lhs.numerator + rhs.numerator, 0), false);
+        }
+        
+        let n1 = Int.multiplyWithOverflow(lhs.numerator, rhs.denominator);
+        let n2 = Int.multiplyWithOverflow(rhs.numerator, lhs.denominator);
+        let nTemp = Int.addWithOverflow(n1.0, n2.0);
+        
+        let dTemp = Int.multiplyWithOverflow(lhs.denominator, rhs.denominator);
+        
+        return (Rational(nTemp.0, dTemp.0), n1.1 || n2.1 || nTemp.1 || dTemp.1);
+    }
+    
+    static func - (lhs: Rational, rhs: Rational) -> Rational {
+        guard lhs.isFinite() && rhs.isFinite() else {
+            return Rational(lhs.numerator - rhs.numerator, 0);
+        }
+        
+        let lcm = Int.lcm(lhs.denominator, rhs.denominator);
+        return Rational((lhs.numerator * (lcm / lhs.denominator)) - (rhs.numerator * (lcm / rhs.denominator)), lcm);
+    }
+    
+    static func subtractWithOverflow(_ lhs: Rational, _ rhs: Rational) -> (Rational, overflow: Bool) {
+        guard lhs.isFinite() && rhs.isFinite() else {
+            return (Rational(lhs.numerator - rhs.numerator, 0), false);
+        }
+
+        let n1 = Int.multiplyWithOverflow(lhs.numerator, rhs.denominator);
+        let n2 = Int.multiplyWithOverflow(rhs.numerator, lhs.denominator);
+        let nTemp = Int.subtractWithOverflow(n1.0, n2.0);
+        
+        let dTemp = Int.multiplyWithOverflow(lhs.denominator, rhs.denominator);
+        
+        return (Rational(nTemp.0, dTemp.0), n1.1 || n2.1 || nTemp.1 || dTemp.1);
+    }
+
+    static func * (lhs: Rational, rhs: Rational) -> Rational {
+        guard lhs.isFinite() && rhs.isFinite() else {
+            return Rational(lhs.numerator * rhs.numerator, 0);
+        }
+        
+        let gcd1 = Int.gcd(lhs.numerator, rhs.denominator);
+        let gcd2 = Int.gcd(lhs.denominator, rhs.numerator);
+        
+        return Rational((lhs.numerator / gcd1) * (rhs.numerator / gcd2),
+                        (lhs.denominator / gcd2) * (rhs.denominator / gcd1));
+    }
+    
+    static func multiplyWithOverflow(_ lhs: Rational, _ rhs: Rational) -> (Rational, overflow: Bool) {
+        guard lhs.isFinite() && rhs.isFinite() else {
+            return (Rational(lhs.numerator * rhs.numerator, 0), false);
+        }
+
+        let gcd1 = Int.gcd(lhs.numerator, rhs.denominator);
+        let gcd2 = Int.gcd(lhs.denominator, rhs.numerator);
+        
+        let nTemp = Int.multiplyWithOverflow(lhs.numerator / gcd1, rhs.numerator / gcd2);
+        let dTemp = Int.multiplyWithOverflow(lhs.denominator / gcd2, rhs.denominator / gcd1);
+        
+        return (Rational(nTemp.0, dTemp.0), nTemp.1 || dTemp.1);
+    }
+    
+    static func / (lhs: Rational, rhs: Rational) -> Rational {
+        return lhs * rhs.inverse();
+    }
+
+    static func divideWithOverflow(_ lhs: Rational, _ rhs: Rational) -> (Rational, overflow: Bool) {
+        return multiplyWithOverflow(lhs, rhs.inverse());
+    }
+
+    static func % (lhs: Rational, rhs: Rational) -> Rational {
+        guard lhs.isFinite() && rhs.isFinite() else {
+            return Rational(lhs.numerator % rhs.numerator, 0);
+        }
+
+        let lcm = Int.lcm(lhs.denominator, rhs.denominator);
+        return Rational((lhs.numerator * (lcm / lhs.denominator)) % (rhs.numerator * (lcm / rhs.denominator)), lcm);
+    }
+    
+    static func remainderWithOverflow(_ lhs: Rational, _ rhs: Rational) -> (Rational, overflow: Bool) {
+        guard lhs.isFinite() && rhs.isFinite() else {
+            return (Rational(lhs.numerator % rhs.numerator, 0), false);
+        }
+
+        let n1 = Int.multiplyWithOverflow(lhs.numerator, rhs.denominator);
+        let n2 = Int.multiplyWithOverflow(rhs.numerator, lhs.denominator);
+        let nTemp = Int.remainderWithOverflow(n1.0, n2.0);
+        
+        let dTemp = Int.multiplyWithOverflow(lhs.denominator, rhs.denominator);
+        
+        return (Rational(nTemp.0, dTemp.0), n1.1 || n2.1 || nTemp.1 || dTemp.1);
+    }
+    
+    func toIntMax() -> IntMax {
+        return IntMax(numerator / denominator);
+    }
+}
+
+// MARK: Comparable
+extension Rational : Comparable {
+    static func < (lhs: Rational, rhs: Rational) -> Bool {
+        return lhs.numerator * rhs.denominator < rhs.numerator * lhs.denominator;
+    }
+
+    static func == (lhs: Rational, rhs: Rational) -> Bool {
+        return lhs.numerator * rhs.denominator == rhs.numerator * lhs.denominator;
+    }
+}
+
+// MARK: Hashable
+extension Rational : Hashable {
+    var hashValue : Int {
+        return numerator ^ denominator;
+    }
+}
+
+// MARK: ExpressibleByIntegerLiteral
+extension Rational : ExpressibleByIntegerLiteral {
+    init(integerLiteral value: IntegerLiteralType) {
+        self.init(value, 1);
+    }
+}
+
+// MARK: ExpressibleByFloatLiteral
+extension Rational : ExpressibleByFloatLiteral {
+    init(floatLiteral value: FloatLiteralType) {
+        let r = Double.bestRationalApproxiation(Double(value));
+        
+        numerator = r.numerator;
+        denominator = r.denominator;
+    }
+}
+
+// MARK: CustomStringConvertible
+extension Rational : CustomStringConvertible {
+    var description : String {
+        return "\(numerator)/\(denominator)";
+    }
+}
+
+extension Integer {
+    /// Find the Greatest Common Denominator of two Integers
+    /// - Parameters:
+    ///     - a: First Integer
+    ///     - b: Second Integer
+    /// - Returns: The GCD of the given Integers
+    static func gcd(_ a: Self, _ b: Self) -> Self {
+        return b == 0 ? a : Self.gcd(b, a % b);
+    }
+    
+    /// Find the Least Common Multiple of the two Integers
+    /// - Parameters:
+    ///     - a: First Integer
+    ///     - b: Second Integer
+    /// - Returns: The LCM of the given Integers
+    static func lcm(_ a: Self, _ b: Self) -> Self {
+        if a == 0 || b == 0 {
+            return 0;
+        }
+        
+        return (a / gcd(a, b)) * b;
+    }
+}
+
+extension Double {
+    /// Initialize a Double from a Rational
+    /// - Note: If the deonominator is zero, the Double will be initialized to +/- infinity
+    init(_ number: Rational) {
+        if number.isFinite() {
+            self = Double(number.numerator) / Double(number.denominator);
+        } else if number.isInfinite() {
+            self = number.numerator > 0 ? Double.infinity : -Double.infinity;
+        } else {
+            self = Double.nan;
+        }
+    }
+
+    /// Return the closest Rational within a given error with the smallest possible denominator
+    /// - Parameters:
+    ///     - number: The number to be approximated
+    ///     - eps: The error target. Default value is 1.0e-15.
+    /// - Returns: The Rational with smallest denominsator and within **eps** of number
+    /// - Note: Because the numerator and denominator of Rational are Int, it is not always possible to meet the **eps**
+    ///         target. If **eps** cannot be met, the closest Rational with numerator and denominator fittable within an
+    ///         Int is returned.
+    static func bestRationalApproxiation(_ number: Double, _ eps: Double = 1.0e-15) -> Rational {
+        guard number.isFinite else {
+            return Rational(1, 0);
+        }
+        
+        var nTemp = abs(number);
+        
+        var p1 = 0.0, p2 = 1.0;
+        var q1 = 1.0, q2 = 0.0;
+        
+        repeat {
+            let a = floor(nTemp);
+            
+            let pTemp = a * p2 + p1;
+            let qTemp = a * q2 + q1;
+            
+            if (pTemp > Double(Int.max) || pTemp < Double(Int.min) || qTemp > Double(Int.max) || qTemp < Double(Int.min)) {
+                break;
+            }
+            
+            p1 = p2; p2 = pTemp;
+            q1 = q2; q2 = qTemp;
+
+            nTemp = 1.0 / (nTemp - a)
+
+            if nTemp.isNaN || nTemp.isInfinite {
+                break;
+            }
+        } while abs((p2 / q2) - number) > eps
+        
+        return Rational(Int(number < 0 ? -p2 : p2), Int(q2));
+    }
+}
--- RationalTests/RationalTests.swift
+++ RationalTests/RationalTests.swift
@@ -21,16 +21,137 @@ class RationalTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitWithNumerator() {
+        let nTest = 2;
+        let rTest = Rational(nTest);
+        XCTAssert(rTest.numerator == nTest, "Incorrect Numerator \(rTest.numerator)");
+        XCTAssert(rTest.denominator == 1, "Incorrect Denominator \(rTest.denominator)");
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testInitWithNumeratorAndDenominator() {
+        let nTest = 4;
+        let dTest = 8;
+        
+        let rTest = Rational(nTest, dTest);
+        XCTAssert(rTest.numerator == 1, "Incorrect Numerator \(rTest.numerator)");
+        XCTAssert(rTest.denominator == 2, "Incorrect Denominator \(rTest.denominator)");
     }
     
+    func testInverse() {
+        let r = Rational(1, 2);
+        let rInv = r.inverse();
+        
+        XCTAssert(rInv.numerator == r.denominator && rInv.denominator == r.numerator,
+                  "Incorrect Inverse: \(r) | \(rInv)");
+    }
+    
+    func testIsFinite() {
+        let rFin = Rational(2, 1);
+        let rInf = Rational(2, 0);
+        
+        XCTAssert(rFin.isFinite(), "Incorrect Finite Response \(rFin)");
+        XCTAssert(!rInf.isFinite(), "Incorrect Finite Response \(rInf)");
+    }
+
+    func testIsInfinite() {
+        let rFin = Rational(2, 1);
+        let rInf = Rational(2, 0);
+        
+        XCTAssert(!rFin.isInfinite(), "Incorrect Finite Response \(rFin)");
+        XCTAssert(rInf.isInfinite(), "Incorrect Finite Response \(rInf)");
+    }
+
+    func testNan() {
+        let rFin = Rational(2, 1);
+        let rNaN = Rational(0, 0);
+        
+        XCTAssert(!rFin.isNaN(), "Incorrect NaN Response \(rFin)");
+        XCTAssert(rNaN.isNaN(), "Incorrect NaN Response \(rNaN)");
+    }
+    
+    func testAddition() {
+        let rOne = Rational(1, 0);
+        let rTwo = Rational(3, 4);
+        let rThree = rOne + rTwo;
+        
+        XCTAssert(rThree.numerator == 1, "Incorrect Numerator \(rThree.numerator)");
+        XCTAssert(rThree.denominator == 0, "Incorrect Denominator \(rThree.denominator)");
+        
+        let rFin = Rational(2, 3);
+        let rInf = Rational(-4, 0);
+        let rTot = rFin + rInf;
+        
+        XCTAssert(rTot.numerator == 1 && rTot.denominator == 0, "Incorrect Infinite Addition \(rTot)");
+    }
+
+    func testSubtraction() {
+        let rOne = Rational(1, 2);
+        let rTwo = Rational(-3, 4);
+        let rThree = rOne - rTwo;
+        
+        XCTAssert(rThree.numerator == 5, "Incorrect Numerator \(rThree.numerator)");
+        XCTAssert(rThree.denominator == 4, "Incorrect Denominator \(rThree.denominator)");
+    }
+
+    func testMultiplication() {
+        let rOne = Rational(1, -2);
+        let rTwo = Rational(-3, 4);
+        let rThree = rOne * rTwo;
+        
+        XCTAssert(rThree.numerator == 3, "Incorrect Numerator \(rThree.numerator)");
+        XCTAssert(rThree.denominator == 8, "Incorrect Denominator \(rThree.denominator)");
+    }
+
+    func testDivision() {
+        let rOne = Rational(1, -2);
+        let rTwo = Rational(-3, 4);
+        let rThree = rOne / rTwo;
+        
+        XCTAssert(rThree.numerator == 2, "Incorrect Numerator \(rThree.numerator)");
+        XCTAssert(rThree.denominator == 3, "Incorrect Denominator \(rThree.denominator)");
+    }
+    
+    func testLessThan() {
+        let rOne = Rational(4, 8);
+        let rTwo = Rational(3, 4);
+        let rInf = Rational(1, 0);
+        let rNegInf = Rational(-1, 0);
+        
+        XCTAssert(rOne < rTwo, "Incorrect less-than: \(rOne) !< \(rTwo)");
+        XCTAssert(rOne < rInf, "Incorrect less-than: \(rOne) !< \(rInf)");
+        XCTAssert(rNegInf < rTwo, "Incorrect less-than: \(rNegInf) !< \(rTwo)")
+    }
+
+    func testGCD() {
+        let a = 210;
+        let b = 165;
+    
+        XCTAssert(Int.gcd(a, b) == 15, "Incorrect GCD");
+    }
+    
+    func testPrintRational() {
+        let rOne = Rational(3,2);
+        let rTwo = Rational(-3,2);
+        
+        print(rOne);
+        print(rTwo);
+        
+        let s = "rOne: \(rOne)";
+        print(s);
+    }
+    
+    func testBestRationalApproximation() {
+        let d = 123417.0 / 23572.0;
+        let r = Double.bestRationalApproxiation(d);
+
+        XCTAssert(abs(d-(Double(r.numerator)/Double(r.denominator))) < 1e-15, "Incorrect rational approximation");
+    }
+    
+    func testDoubleFromRational() {
+        let r = Rational(-4, 3);
+        
+        let d = Double(r);
+        
+        XCTAssert(d == -4.0 / 3.0, "\(r) did not convert correctly: \(d)");
+    }
 }
